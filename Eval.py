50c53ee278c07a6b72e51791dd05ec382ac5e32f
Thiviru <thiviru1000@gmail.com>
2025-07-10 18:38:51 +1000

    added mountain car code. commented out cartpole

--- Eval.py
+++ Eval.py
@@ -26,6 +26,33 @@ def render_episode(agent, model_path, num_episodes):
         print(f"Episode {episode + 1} reward: {total_reward}")
     env.close()
 
+# ! ------------------------------------------ CARTPOLE ----------------------------------------------
+
+# if __name__ == "__main__":
+#     # get state and action sizes # ! Change hard coded values if a new environment is used
+#     state_size = 4
+#     action_size = 2
+
+#     # create agent # ! Make sure to copy the exact agent instance used for training. epsilon value can be changed.
+#     agent = Agent(
+#         state_size=state_size,
+#         action_size=action_size,
+#         n_hidden=100,
+#         memory_maxlen=2000,
+#         gamma=0.99,
+#         epsilon=0, # exploitation only during evaluation
+#         epsilon_min=0.01,
+#         epsilon_decay=0.995
+#     )
+
+#     # Load the model and render episodes
+#     ENV_NAME = 'CartPole-v1'
+#     model_path = f"RL/DQN/Models/{ENV_NAME}/FINAL_model.pt"
+
+#     render_episode(agent, model_path, num_episodes=2)
+
+# ! ------------------------------------------ Mountain CAR ----------------------------------------------
+
 if __name__ == "__main__":
     # get state and action sizes # ! Change hard coded values if a new environment is used
     state_size = 4
--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ from Env import Env
 import matplotlib
 matplotlib.use('TkAgg')  # Use TkAgg backend for plotting
 import matplotlib.pyplot as plt
+import os
 
 def plot_rewards(trainer_obj: Trainer, env_name: str):
     rewards_history = trainer_obj.total_rewards_history
@@ -22,9 +23,63 @@ def plot_rewards(trainer_obj: Trainer, env_name: str):
     plt.savefig(f"RL/DQN/{env_name.upper()} -> total_rewards_per_episode.png")
     plt.show()
 
+# ! ------------------------------------------ CARTPOLE ----------------------------------------------
+
+# if __name__ == "__main__":
+#     ENV_NAME = "CartPole-v1" # ! change name if a different env is used
+#     MODEL_PATH = f"RL/DQN/Models/{ENV_NAME}" 
+
+    # # create dir if not exists
+    #     if not os.path.exists(MODEL_PATH):
+    #         os.makedirs(MODEL_PATH)
+
+#     # create the environment
+#     env = Env(ENV_NAME).env
+
+#     # get state and action sizes
+#     state_size = env.observation_space.shape[0]
+#     action_size = env.action_space.n
+
+#     # create agent
+#     agent = Agent(
+#         state_size=state_size,
+#         action_size=action_size,
+#         n_hidden=100,
+#         memory_maxlen=2000,
+#         gamma=0.99,
+#         epsilon=1.0,
+#         epsilon_min=0.01,
+#         epsilon_decay=0.995
+#     )
+
+#     # create trainer
+#     trainer = Trainer(
+#         agent=agent,
+#         env=env,
+#         batch_size=32,
+#         n_episodes=2000,
+#         lr=0.001,
+#         optimizer="adam",
+#         target_update_freq=5,
+#         save_freq=100,
+#         model_save_path=MODEL_PATH
+#     )
+
+#     # train the agent
+#     trainer.run_agent()
+
+#     # plot the rewards
+#     plot_rewards(trainer, ENV_NAME)
+
+# ! ------------------------------------------ Mountain CAR ----------------------------------------------
+
 if __name__ == "__main__":
-    ENV_NAME = "CartPole-v1" # ! change name if a different env is used
+    ENV_NAME = "MountainCar-v0" # ! change name if a different env is used
     MODEL_PATH = f"RL/DQN/Models/{ENV_NAME}" 
+
+    # create dir if not exists
+    if not os.path.exists(MODEL_PATH):
+        os.makedirs(MODEL_PATH)
 
     # create the environment
     env = Env(ENV_NAME).env
@@ -41,7 +96,7 @@ if __name__ == "__main__":
         memory_maxlen=2000,
         gamma=0.99,
         epsilon=1.0,
-        epsilon_min=0.01,
+        epsilon_min=0.001,
         epsilon_decay=0.995
     )
 
