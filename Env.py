a77396b483e28de2a1d46e28ca294da94f05dc9c
Thiviru <thiviru1000@gmail.com>
2025-07-10 18:17:38 +1000

    added another game env

--- Env.py
+++ Env.py
@@ -14,6 +14,20 @@ class CartPole():
         print("Actions: ", self.env.action_space)
         print("Action size: ", self.env.action_space.n)
 
+class MountainCar():
+    def __init__(self, name):
+        self.name = name
+        self.env = self.make_env()
+
+    def make_env(self):
+        return gym.make(self.name)
+
+    def info(self):
+        print("States: ", self.env.observation_space)
+        print("State size: ", self.env.observation_space.shape[0])
+        print("Actions: ", self.env.action_space)
+        print("Action size: ", self.env.action_space.n)
+
 if __name__ == "__main__":
-    env = CartPole("CartPole-v1")
+    env = CartPole("MountainCar-v0")
     env.info()
